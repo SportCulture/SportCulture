0a5929cebe052196b3061fc493ce729bde1d7b95
SportCulture <107212580+SportCulture@users.noreply.github.com>
2022-06-19 23:00:32 +0200

    Add files via upload

---
+++ footeur.php
@@ -0,0 +1,33 @@
+<?php
+echo "<footer class='bg-dark'>
+     <div id='foot1'>
+       <img src='logo/icon-site-1.png' alt='' height='150px'>
+       <div>
+         <h4><span>La Cité Univercitaire Chlef</span> Sidi Amar</h4>
+         <h1>Espace des activités de la résidence Chlef</h1>
+       </div>
+     </div>
+     <br />
+     <div id='foot2'>
+       <h2 class='text-uppercase'>à Propos</h2>
+       <p>Il existe de nombreuses activités qui neécessient une inscription via le site, il est nécessaire
+         de suivre les conditions montionnées, ainsi pour les concours, y compris pour la participation
+         et d'autres sans cela.</p>
+     </div>
+     <hr />
+     <div id='foot3'>
+       <span>Restons en contact</span>
+       <a href='CONTACTER.php' target='_blank'>
+         <div><img src='social/email.png' alt=''>Nous contacter</div>
+       </a>
+       <a href='https://web.facebook.com/ru2000litschlef' target='_blank'>
+         <div><img src='social/facebook.png' alt=''>La cité Univercitaire Chlef-Sidi Amar</div>
+       </a>
+       <a href=''>
+         <div><img src='social/insta.png' alt=''>@citeuniversitairechlef</div>
+       </a>
+       <a href=''>
+         <div><img src='social/twittre.png' alt=''>cucs_dz</div>
+       </a>
+     </div>
+   </footer>";
